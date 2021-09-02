293de7a445efab56a4358047550e3e21e59784e5
leisiji <xuelinye9@gmail.com>
2021-09-02 16:23:52 +0800

    fix wrong preview

--- lua/fzf_utils/float_preview.lua
+++ lua/fzf_utils/float_preview.lua
@@ -114,7 +114,7 @@ function M.close_preview_win()
   end
 end
 
-M.vimgrep_preview = u.expect_key..'--preview='..require('fzf.actions').action(function(s, _, _)
+M.vimgrep_preview = u.expect_key..' --preview='..require('fzf.actions').action(function(s, _, _)
   local c = u.parse_vimgrep(s[1])
   open_floating_win(c[1], c[2])
   return ""
