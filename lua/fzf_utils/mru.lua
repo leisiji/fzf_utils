0f91e448235ff91f793b178ba7d3bdc5e1d5cbf2
leisiji <xuelinye9@gmail.com>
2022-05-15 17:26:46 +0800

    refract; limit to mru entry num

--- lua/fzf_utils/mru.lua
+++ lua/fzf_utils/mru.lua
@@ -5,25 +5,30 @@ local fn = vim.fn
 local a = require('plenary.async_lib')
 local u = require('fzf_utils.utils')
 local uv = a.uv
-local mru = string.format('%s/%s', fn.stdpath('cache'), 'fzf_mru')
-local fzf_mru_size = 64 * 1024
-local lock = false
+local fzf_mru = {
+  mtime = 0,
+  cache = "",
+  nums = 1024,
+  lock = false,
+  file = string.format('%s/%s', fn.stdpath('cache'), 'fzf_mru')
+}
 
 -- fzf_mru_mtime, fzf_mru_cache is for cache.
 -- When 'fzf_mru' is modified in another vim, it should update the cache
 local function add_file(f)
   a.async_void(function ()
-    lock = true
+    fzf_mru.lock = true
     local data
     local res = f .. '\n'
     local i, p = 1, 1
-    local _, fd = a.await(uv.fs_open(mru, 'r+', 438))
+    local _, fd = a.await(uv.fs_open(fzf_mru.file, 'r+', 438))
     local _, stat = a.await(uv.fs_fstat(fd))
+    local nums = 0
 
-    if vim.g.fzf_mru_mtime ~= stat.mtime.sec then
+    if fzf_mru.mtime ~= stat.mtime.sec then
       _, data = a.await(uv.fs_read(fd, stat.size, 0))
     else
-      data = vim.g.fzf_mru_cache
+      data = fzf_mru.cache
     end
 
     while i <= #data do
@@ -33,62 +38,65 @@ local function add_file(f)
           break
         end
         p = i + 1
+        nums = nums + 1
       end
       i = i + 1
     end
 
-    -- if file not found in mru, then check size and append
+    -- if file not found in mru, then check nums and append
     if i > #data then
-      i = fzf_mru_size - #data
-      if i < #res then
-        while string.sub(res, i, i) ~= '\n' do
-          i = i - 1
-          if i == 0 then
-            vim.notify(string.format('Corrupted mru files: %s', mru))
-            a.await(uv.fs_close(fd))
-            lock = false
-            return
+      if nums > fzf_mru.nums then
+        local oversize = nums - fzf_mru.nums
+        local j = #data
+        while oversize > 0 do
+          j = j - 1
+          if string.sub(data, j, j) == '\n' then
+            oversize = oversize - 1
           end
         end
-        res = string.sub(res, 1, i) .. data
+        res = res .. string.sub(data, 1, j)
       else
         res = res .. data
       end
     end
 
     a.await(uv.fs_write(fd, res, 0))
+    a.await(uv.fs_ftruncate(fd, #res))
 
     _, stat = a.await(uv.fs_fstat(fd))
-    vim.g.fzf_mru_cache = res
-    vim.g.fzf_mru_mtime = stat.mtime.sec
+    fzf_mru.cache = res
+    fzf_mru.mtime = stat.mtime.sec
     a.await(uv.fs_close(fd))
-    lock = false
+    fzf_mru.lock = false
   end)()
 end
 
 function M.refresh_mru()
-  if lock then
+  if fzf_mru.lock then
     return
   end
 
-  local f = fn.expand('%:p')
-  if fn.filereadable(f) == 0 then
-    return
-  end
+  vim.defer_fn(function ()
+    local f = fn.expand('%:p')
+    if fn.filereadable(f) == 0 then
+      return
+    end
 
-  if fn.filereadable(mru) == 0 then
-    local file = io.open(mru, "a")
-    io.close(file)
-  end
+    if fn.filereadable(fzf_mru.file) == 0 then
+      local file = io.open(fzf_mru.file, "a")
+      io.close(file)
+    end
 
-  add_file(f)
+    add_file(f)
+  end, 500)
+
 end
 
 function M.fzf_mru()
   coroutine.wrap(function ()
-    local choices = require('fzf').fzf('cat ' .. mru, u.expect_key())
+    local choices = require('fzf').fzf('cat ' .. fzf_mru.file, u.expect_key())
     if choices[1] == "ctrl-r" then
-      os.remove(mru)
+      os.remove(fzf_mru.file)
       vim.schedule(M.fzf_mru)
     else
       u.handle_key(choices[1], choices[2], nil, nil)
--- plugin/fzf_utils.lua
+++ plugin/fzf_utils.lua
@@ -5,7 +5,7 @@ end, { nargs = "+", complete = "dir" })
 
 local group = "fzf_utils"
 a.nvim_create_augroup(group, { clear = true })
-a.nvim_create_autocmd({ "TabEnter" }, {
+a.nvim_create_autocmd({ "TabNew" }, {
   callback = require("fzf_utils.mru").refresh_mru,
   group = group,
 })
